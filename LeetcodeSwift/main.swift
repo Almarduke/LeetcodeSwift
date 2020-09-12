1e1c94af1631fa7eb7cac10ba915ea2cf086ebb1
Sephidator <Sephidator@gmail.com>
2020-09-12 21:18:09 +0800

    Initial Commit

---
+++ LeetcodeSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetcodeSwift
+//
+//  Created by Almarduke on 2020/9/12.
+//  Copyright © 2020 Almarduke. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
